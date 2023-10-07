029dcfa70af012286ea0359725c484b16d114aea
rroopkumar <rpkmrroopkumar8@gmail.com>
2023-10-08 01:29:05 +0530

    updated the project related model and controller

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,10 +1,14 @@
 const projectMiddleware = require("../middlewares/projectMiddleware");
+const userMiddleware = require("../middlewares/userMiddleware");
+const intakeFormMiddleware = require("../middlewares/intakeFormMiddleware");
 
 const createProject = (req, res, next) =>{
-    let {projectName,projectType,clientPhNumber,address,city,state,country,zipcode,startDate,endDate,moveDate} =req.body;
+    // doc_link category need Done
+    // Need to add the file uload logic
+    let {projectName,projectType,clientPhNumber,address,city,state,country,zipcode,startDate,endDate,moveDate, documents, serviceNeeded} =req.body;
     let userId = req.user._id;
-    if(!projectName||!projectType||!clientPhNumber||!address||!city||!state||!country||!zipcode||!startDate||!endDate||!moveDate||!userId) throw({message:"Required fields are missing"});
-    projectMiddleware.createRecord({projectName,projectType,clientPhNumber,address,city,state,country,zipcode,startDate,endDate,moveDate,userId}).then(data =>{
+    if(!projectName||!projectType||!clientPhNumber||!address||!city||!state||!country||!zipcode||!startDate||!endDate||!moveDate||!documents||!serviceNeeded ||!userId) throw({message:"Required fields are missing"});
+    projectMiddleware.createRecord({projectName,projectType,clientPhNumber,address,city,state,country,zipcode,startDate,endDate,moveDate,userId, documents, serviceNeeded}).then(data =>{
         res.json(data);
         // res.json({status:true, data});
     }).catch(err=>{
@@ -21,9 +25,9 @@ const getProject = async(req, res, next) =>{
     filterQuery ={
         _id:req.params.id
     }
-    let dataaaa = await projectMiddleware.getSingleRecord({filterQuery, projectQuery});
+    // let dataaaa = await projectMiddleware.getSingleRecord({filterQuery, projectQuery});
 
-    console.log("========dataaaa",dataaaa)
+    // console.log("========dataaaa",dataaaa)
     projectMiddleware.getSingleRecord({filterQuery, projectQuery}).then(data =>{
         res.json(data);
         // res.json({status:true, data});
@@ -62,23 +66,52 @@ const updateProject = (req, res, next) =>{
     });
 }
 
-const publishProject = (req, res, next) =>{
-    let filterQuery = req.query
+const publishProject = async (req, res, next) => {
+  try {
+    let filterQuery = req.query;
     let updateObj = {
-        status:active
-    }
-    filterQuery={
-        _id:req.params.id
-    }
+      status: active,
+    };
+    filterQuery = {
+      _id: req.params.id,
+    };
+    let servicesArray =[] //need to fetch from the project intakeform
+    let intakefilterQuery = {projectId:req.params.id}
+    let intakeProjectQuery = {serviceNeeded:1}
+    let allIntakeForms = await intakeFormMiddleware.getAllRecords({
+        intakefilterQuery,
+        intakeProjectQuery,
+    })
+    if(allIntakeForms.status && allIntakeForms.data.length == 0) throw({message: "Cannot Publish a project without a Intakeform" });
+    servicesArray = allIntakeForms.data[0].serviceNeeded;
+
+    let userfilterQuery = { servicesProvided: { $in: servicesArray } };
+    let userProjectQuery = { email: 1 };
+    let serviceProviderEmails = await userMiddleware.getAllRecords({
+        userfilterQuery,
+        userProjectQuery,
+    });
+    let providerEmails = serviceProviderEmails.map(value => value.email);
+    console.log("providerEmails=====", providerEmails);
+    
     // TODO need to add the mailing logic to send mails to all contractors with service provided
-    projectMiddleware.updateRecord({filterQuery, updateObj}).then(data =>{
+    // mailing logic
+    projectMiddleware
+      .updateRecord({ filterQuery, updateObj })
+      .then((data) => {
         res.json(data);
         // res.json({status:true, data});
-    }).catch(err=>{
-        console.log("err===",err);
-        res.json({status:false, message:err.message});
-    });
-}
+      })
+      .catch((err) => {
+        console.log("err===", err);
+        res.json({ status: false, message: err.message });
+      });
+  } catch (err) {
+    console.log("err===", err);
+    res.json({ status: false, message: err.message });
+  }
+};
+
 const deleteProject = (req, res, next) =>{
     let filterQuery = req.query
     let updateObj = {
