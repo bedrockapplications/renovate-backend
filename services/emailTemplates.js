e2751c94ad3e010bca53f7bc032483bee405ea97
Nitin Kumar Posani <107908123+PosaniNitinkumar@users.noreply.github.com>
2023-10-08 01:42:36 +0530

    template

--- services/emailTemplates.js
+++ services/emailTemplates.js
@@ -65,7 +65,70 @@ const email_templates = {
     </body>
     </html>
     `
-  }
+  },
+  //send mails to all contractors with service provided
+  "publish_project": {
+    subject: "Bedrock - New Project",
+    text: (template_data) => `Hi,\n\nA new project has been published. Please check your dashboard.`,
+    html: (template_data) => `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                background-color: #f4f4f4;
+                margin: 0;
+                padding: 0;
+            }
+    
+            .container {
+                max-width: 600px;
+                margin: 20px auto;
+                background-color: #ffffff;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+    
+            h1 {
+                color: #333;
+            }
+    
+            p {
+                color: #555;
+            }
+    
+            a {
+                color: #007BFF;
+                text-decoration: none;
+            }
+    
+            .btn {
+                display: inline-block;
+                padding: 10px 20px;
+                margin-top: 15px;
+                background-color: #007BFF;
+                color: #fff;
+                text-decoration: none;
+                border-radius: 5px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div class="container">
+            <h1>Bedrock - New Project</h1>
+            <p>Hi,</p>
+            <p>A new project has been published. Please check your dashboard for more details.</p>
+            <a href="#" class="btn">Visit Dashboard</a>
+        </div>
+    </body>
+    
+    </html>`    
+
   
 }
 
