c281ac98fd13dfff20421df2bc52ab80f5b6fbed
rroopkumar <rpkmrroopkumar8@gmail.com>
2023-10-08 20:02:48 +0530

    added the validation middleware function to project routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const projectController = require('../controllers/projectController');
 const auth = require('../middlewares/tokenValidate');
 
-router.post("/add",auth.tokenValidation,projectController.createProject)
+router.post("/add",auth.tokenValidation,auth.ownerValidation,projectController.createProject)
 router.get("/get",projectController.getAllProject)
 router.get("/get/:id",projectController.getProject)
-router.put("/update/:id",auth.tokenValidation,projectController.updateProject)
-router.put("/publish/:id",auth.tokenValidation,projectController.publishProject)
-router.delete("/delete/:id",auth.tokenValidation,projectController.deleteProject)
+router.put("/update/:id",auth.tokenValidation,auth.ownerValidation,projectController.updateProject)
+router.put("/publish/:id",auth.tokenValidation,auth.ownerValidation,projectController.publishProject)
+router.delete("/delete/:id",auth.tokenValidation,auth.ownerValidation,projectController.deleteProject)
 
 module.exports = router;
