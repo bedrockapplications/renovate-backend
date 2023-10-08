f2ccd034443592cfeb5c0eb2d1e159e52716bb44
rroopkumar <rpkmrroopkumar8@gmail.com>
2023-10-09 00:04:26 +0530

    updated the fileupload logic for doc

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const auth = require('../middlewares/tokenValidate');
+const {s3MultipleFileUpload} = require('../utils/s3fileUpload');
 
 router.post("/add",auth.tokenValidation,auth.ownerValidation,s3MultipleFileUpload.array('file', 3),projectController.createProject)
 router.get("/get",projectController.getAllProject)
